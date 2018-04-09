3c9eea484b0f8356ea8f25db24379ef27c5faaa9
sunguangrui <277061708@qq.com>
2018-04-09 11:19:37 +0800

    modify MainAc

--- app/src/main/java/com/example/iris/product/MainActivity.java
+++ app/src/main/java/com/example/iris/product/MainActivity.java
@@ -51,6 +51,7 @@ public class MainActivity extends BaseActivity implements
     @Override
     public void setLayout() {
         setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_main_test);
     }
 
     @Override
